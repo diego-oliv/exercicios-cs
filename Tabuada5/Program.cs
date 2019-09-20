626ce2b12fcba1721b092462cb5fed2b8e2d2917
diego-oliv <diegofreitas.oliveira01@gmail.com>
2019-09-20 11:18:14 -0300

    Upload dos Exercícios

---
+++ Tabuada5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tabuada5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 50; i = i + 5)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}

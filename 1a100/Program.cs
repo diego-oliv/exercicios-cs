a1f0ef11b7708e805ab8002d789169ce5b19a7b5
diego-oliv <diegofreitas.oliveira01@gmail.com>
2019-09-20 11:00:57 -0300

    Atualização / Upload de Arquivos

---
+++ 1a100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1a100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i += 2)
+            {
+                Console.Write(i + " ");
+            }        
+        }
+    }
+}
---
+++ ParImpar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            do {
+                Console.Write("Digite um número (Press '0' to cancel): ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num != 0)
+                    if (num % 2 == 0)
+                        {
+                            Console.WriteLine("Esse número é par");                
+                        } else {
+                            Console.WriteLine("Esse número é ímpar");
+                        } 
+                } while (num != 0);
+        }
+    }
+}

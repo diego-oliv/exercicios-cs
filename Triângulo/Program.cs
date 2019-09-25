9eaccce9d03ee91f720cabba42e51e6a340f7ba8
Diego De Oliveira Freitas <35883602800@info.local>
2019-09-25 11:24:39 -0300

    Dia 25

---
+++ Triângulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triângulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a altura do triângulo: ");
+            int alturaT = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de triângulos: ");
+            int qteT = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qteT; i++){
+                string estrelas = "";
+                for(int j = 0; j < alturaT; j++){
+                    estrelas += "*";
+                    Console.WriteLine(estrelas);
+                }
+                Console.WriteLine();            
+            }
+        }
+    }
+}
---
+++ vetor1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace vetor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double par = 0;
+            double impar = 0;
+            int [] vetor = new int [6];
+            for (int i = 0; i < 6; i++){
+                if (i % 2 == 0){
+                    par++;
+                }else{
+                    impar++;
+                }                
+            }
+            Console.WriteLine("Número de pares é: " + par);
+            Console.WriteLine("Número de ímapres é: " + impar);  
+        }
+    }
+}
